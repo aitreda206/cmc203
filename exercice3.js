98c2a4455b33a9d0cc0c12b77f570a2c3cc170b7
aitreda206 <2006112500157@ofppt-edu.ma>
2025-12-05 16:15:16 +0100

    exercice 3 javascript dans la branche dev

---
+++ exercice3.js
@@ -0,0 +1,11 @@
+const inscriptions=[
+{id:10,nom:'Rami',filiere:'DEV'},
+{id:11,nom:'Kamali',filiere:'DEV'},
+{id:12,nom:'Fahmi',filiere:'DEV'},
+{id:13,nom:'Chaouki',filiere:'DEV'}
+];
+function insérer(i,n,f){
+    inscriptions.splice(inscriptions.length,0,{id:i,nom:n,filiere:f})
+}
+insérer(14,"reda","DEV")
+console.log(inscriptions)
